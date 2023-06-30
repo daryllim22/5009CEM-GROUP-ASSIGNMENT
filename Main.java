4f6338e1e9c4d3788c37afb5a38114af6405cb2c
SJ-earworm <P19011819@gmail.com>
2023-07-01 01:35:22 +0800

    uploading what I currently have for ApartmentManagementInterface

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//package pkg5009cem_assignment;
+
+/**
+ *
+ * @author sjjde
+ */
+//public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+/*    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("herro i testing first i'm so rusty");
+        
+        haiyatestingagainplswork PLS = new haiyatestingagainplswork();
+        PLS.init();
+    }
+    
+}*/
